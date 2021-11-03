f0b541a19f47c041dd8603c7a0e6b4b536347970
sdelappe <sdelappe57@gmail.com>
2021-11-03 06:56:48 -0700

    finished exercise

---
+++ ASP .NET Exercise/IProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using ASP_.NET_Exercise.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_.NET_Exercise
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct(int ID);
+
+        public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+    }
+}
